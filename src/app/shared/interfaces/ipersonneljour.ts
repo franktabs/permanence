a460b4fd7834176729890bf1866fe0fe586d6dc2
franktabs <tabuguiafrank@gmail.com>
2023-08-13 05:20:11 +0200

    Ajout des interface ipersonneljour et ipersonnelnuit

---
+++ src/app/shared/interfaces/ipersonneljour.ts
@@ -0,0 +1,9 @@
+import { IApiPersonnel } from "./iapipersonnel";
+import { IPermanence } from "./ipermanence";
+
+export interface IPersonnelJour {
+    id?:number;
+    personnel:IApiPersonnel;
+    permanence?:IPermanence;
+    responsable:boolean;
+}
