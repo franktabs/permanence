d8117b79093c61849f909e3564c8393940099e9c
franktabs <tabuguiafrank@gmail.com>
2023-09-18 06:47:45 +0200

    ajustement style card group personnel

--- src/app/shared/components/card-group-personnel/card-group-personnel.component.ts
+++ src/app/shared/components/card-group-personnel/card-group-personnel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import IGroupe from '../../interfaces/igroupe';
 
 @Component({
   selector: 'app-card-group-personnel',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardGroupPersonnelComponent implements OnInit {
 
+  @Input()
+  public group:IGroupe= {nom:"group", criteres:[], personnels:[]};
   
 
   constructor() { }
--- src/app/shared/interfaces/igroupe.ts
+++ src/app/shared/interfaces/igroupe.ts
@@ -1,7 +1,9 @@
+import { IApiPersonnel } from "./iapipersonnel";
 import ICritere from "./icritere";
 
 
 export default interface IGroupe {
     nom?:string;
+    personnels:IApiPersonnel[]
     criteres:ICritere[]
 }
